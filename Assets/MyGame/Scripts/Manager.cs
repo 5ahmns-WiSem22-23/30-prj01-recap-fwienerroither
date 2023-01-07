59f0388990230ed2b2cfdc6dd42b2fcba1eedf0e
FabioWienerroither <fabio.wienerroither04@htl-salzburg.ac.at>
2023-01-07 12:47:07 +0100

    Background und Time Counter

--- Assets/MyGame/Scripts/Manager.cs
+++ Assets/MyGame/Scripts/Manager.cs
@@ -1,19 +1,57 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Manager : MonoBehaviour
 {
     [SerializeField]
     private float minRange;
+
     [SerializeField]
     private float maxRange;
+
     [SerializeField]
     private GameObject tile;
 
+    [SerializeField]
+    Text tileCountText;
+
     public static int tileCounter;
+
+    float time;
+
+    [SerializeField]
+    float maxTime;
+
+
+    [SerializeField]
+    Text timeText;
+
+
 
     void Start()
     {
         SpawnTile();
+    }
+
+    private void Update()
+    {
+        tileCountText.text = tileCounter.ToString();
+        timeText.text = "Zeit: " + Mathf.Round(maxTime - time).ToString();
+
+        // Timer erstellen/einstellen
+        time += Time.deltaTime;
+
+        //Nach Ablauf der Zeit wird das Spiel neu gestartet
+        if (time >= maxTime)
+        {
+            SceneManager.LoadScene(0);
+            tileCounter = 0;
+        }
+
+        
+
+
     }
 
 
